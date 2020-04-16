23a534d24693c77f7507568ff578830758882f62
Philip Ng <phil__n@hotmail.com>
2020-04-16 17:22:34 -0400

    finished about page

--- hw20-react-portfolio/src/App.js
+++ hw20-react-portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import Contact from "./components/pages/Contact/Contact";
 // import Portfolio from "./components/pages/Portfolio/Portfolio";
 
--- hw20-react-portfolio/src/components/pages/About/About.js
+++ hw20-react-portfolio/src/components/pages/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import Resume from "../../Resume/Resume2020.pdf";
+import ProfilePicture from "./img/about-profilepic.jpg";
 
 function About() {
   return (
@@ -14,32 +14,41 @@ function About() {
       </div>
       <div className="text-container">
         <div class="about-me-text-div">
-          <p>Hello! My name is Philip Ng.</p>
-          <p>
-            While studying History and Education at Queen's University, I became
-            well practiced in analysis, interpretation, research, and
-            multi-faceted political, economic and social evaluations. After
-            graduating, I wanted to use these skills to focus on making people's
-            lives easier. I grew up with a love of puzzles, problem solving and
-            a yearning to understand complex issues. I deeply value innovation
-            and originality and coding was something that encompassed all those
-            things.
-          </p>
-          <p>
-            From my time at UofT Mississauga's Coding Bootcamp, I have
-            experience working with technologies such as: HTML, CSS, Bootstrap,
-            Javascript, JQuery, Node.js, Express, Sequelize, MySQL, MongoDB, and
-            React. I intend to use these skills to build useful websites and
-            applications. Check out some of the projects I've worked on by
-            scrolling further down below!
-          </p>
+          <div className="about-me-content">
+            <div className="image-Div">
+              {" "}
+              <img src={ProfilePicture} id="profilePic" alt="me"></img>
+            </div>
+            <div class="row">
+              <div class="col-lg-12 col-md-12 col-sm-12 col-12 d-block m-auto text-left">
+                <p className="aboutMeP">Hello! My name is Philip Ng.</p>
+                <p className="aboutMeP">
+                  While studying History and Education at Queen's University, I
+                  became well practiced in analysis, interpretation, research,
+                  and multi-faceted political, economic and social evaluations.
+                  After graduating, I wanted to use these skills to focus on
+                  making people's lives easier. I grew up with a love of
+                  puzzles, problem solving and a yearning to understand complex
+                  issues. I deeply value innovation and originality and coding
+                  was something that encompassed all those things.
+                </p>
+                <p className="aboutMeP">
+                  From my time at UofT Mississauga's Coding Bootcamp, I have
+                  experience working with technologies such as: HTML, CSS,
+                  Bootstrap, Javascript, JQuery, Node.js, Express, Sequelize,
+                  MySQL, MongoDB, and React. I intend to use these skills to
+                  build useful websites and applications. Check out some of the
+                  projects I've worked on by scrolling further down below!
+                </p>
+              </div>
+            </div>
+          </div>
           <hr class="dotted" />
           <h2 className="contactH2">
             Here are some of the ways you can contact me:
           </h2>{" "}
           <div className="container">
-            <div className="contactInfoDiv">
-              {" "}
+            <div className="contactInfoDiv text-left m-auto col-lg-6 col-md-6 col-sm-6 col-6">
               <p>
                 <b>Email: </b>
                 <a
